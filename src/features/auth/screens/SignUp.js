2c001cc805b1f08d637458b496a28361996c8fa3
MounicaGit <64646684+MounicaGit@users.noreply.github.com>
2025-12-17 23:40:10 +0530

    Refactor SignUp component for validation and navigations

--- src/features/auth/screens/SignUp.js
+++ src/features/auth/screens/SignUp.js
@@ -4,30 +4,49 @@ import {
   Image,
   // TextInput,
   StyleSheet,
-  SafeAreaView,
   Pressable,
+  ScrollView,
   TouchableOpacity,
+  KeyboardAvoidingView,
 } from 'react-native';
-import { TextInput, Divider, Checkbox } from 'react-native-paper';
+import { TextInput, Divider, Checkbox, Snackbar } from 'react-native-paper';
 import Button from '../../../components/commons/Button';
+import { emailRules, passwordRules, phoneRules, fullNameRules } from '../../../utils/validators/rules';
+import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 import { useNavigation } from '@react-navigation/native';
 
 export default function SignUp() {
+  const [showPassword, setShowPassword] = useState(false);
+  const [showConfirmPassword, setShowConfirmPassword] = useState(false);
+  const { control, handleSubmit, watch, formState: { errors }, } = useForm({ defaultValues: { fullName: '', email: '', phoneNumber: '', password: '', confirmPassword: '', }, mode: 'onSubmit', });
+  const password = watch('password');
   const navigation = useNavigation();
+  const [SnackbarText, setSnackbarText] = useState('');
+  const [agreeTnC, setAgreeTnC] = useState(false);
 
   function renderFullNameTextField() {
     return (
       <View style={styles.textInputView}>
         <Text>Full Name</Text>
-        <TextInput
-          onChangeText={() => {}}
-          style={styles.textInput}
-          placeholder={'Enter your full name'}
-          underlineColor={'transparent'}
-          placeholderTextColor={'grey'}
-          activeUnderlineColor={'transparent'}
-          left={<TextInput.Icon icon={'account'} size={20} color="grey" />}
-        />
+        <Controller
+          control={control}
+          name="fullName"
+          rules={fullNameRules}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              onChangeText={onChange}
+              style={styles.textInput}
+              value={value}
+              placeholder={'Enter your full name'}
+              underlineColor={'transparent'}
+              placeholderTextColor={'grey'}
+              activeUnderlineColor={'transparent'}
+              left={<TextInput.Icon icon={'account'} size={20} color="grey" />}
+            />)}>
+
+        </Controller>
+        {errors.fullName && renderErrorMsg(errors.fullName.message)}
       </View>
     );
   }
@@ -35,16 +54,26 @@ export default function SignUp() {
     return (
       <View style={styles.textInputView}>
         <Text>Phone Number</Text>
-        <TextInput
-          onChangeText={() => {}}
-          style={styles.textInput}
-          placeholder={'Enter your phone number'}
-          underlineColor={'transparent'}
-          placeholderTextColor={'grey'}
-          activeUnderlineColor={'transparent'}
-          left={<TextInput.Icon icon={'phone'} size={20} color="grey" />}
-        />
-      </View>
+        <Controller
+          control={control}
+          name="phoneNumber"
+          rules={phoneRules}
+          render={({ field: { onChange, value } }) => (
+            <TextInput
+              onChangeText={onChange}
+              style={styles.textInput}
+              value={value}
+              keyboardType='phone-pad'
+              placeholder={'Enter your phone number'}
+              underlineColor={'transparent'}
+              placeholderTextColor={'grey'}
+              activeUnderlineColor={'transparent'}
+              left={<TextInput.Icon icon={'phone'} size={20} color="grey" />}
+            />
+          )}>
+        </Controller>
+        {errors.phoneNumber && renderErrorMsg(errors.phoneNumber.message)}
+      </View >
     );
   }
 
@@ -52,15 +81,23 @@ export default function SignUp() {
     return (
       <View style={styles.textInputView}>
         <Text>Email</Text>
-        <TextInput
-          onChangeText={() => {}}
-          style={styles.textInput}
-          placeholder={'Enter your email'}
-          underlineColor={'transparent'}
-          placeholderTextColor={'grey'}
-          activeUnderlineColor={'transparent'}
-          left={<TextInput.Icon icon={'email'} size={20} color="grey" />}
+        <Controller
+          control={control}
+          name="email"
+          rules={emailRules}
+          render={({ field: { onChange, value } }) => (<TextInput
+            onChangeText={onChange}
+            style={styles.textInput}
+            value={value}
+            keyboardType='email-address'
+            placeholder={'Enter your email'}
+            underlineColor={'transparent'}
+            placeholderTextColor={'grey'}
+            activeUnderlineColor={'transparent'}
+            left={<TextInput.Icon icon={'email'} size={20} color="grey" />}
+          />)}
         />
+        {errors.email && renderErrorMsg(errors.email.message)}
       </View>
     );
   }
@@ -70,17 +107,24 @@ export default function SignUp() {
       <View style={styles.textInputView}>
         <Text>Password</Text>
         <View>
-          <TextInput
-            onChangeText={() => {}}
-            style={styles.textInput}
-            placeholder={'Create a password'}
-            placeholderTextColor={'grey'}
-            secureTextEntry={true}
-            underlineColor={'transparent'}
-            right={<TextInput.Icon icon={'eye-off'} size={20} color={'grey'} />}
-            activeUnderlineColor={'transparent'}
-            left={<TextInput.Icon icon="lock" size={20} color={'grey'} />}
-          />
+          <Controller
+            control={control}
+            rules={passwordRules}
+            name='password'
+            render={({ field: { onChange, value } }) => (<TextInput
+              onChangeText={onChange}
+              value={value}
+              style={styles.textInput}
+              placeholder={'Create a password'}
+              placeholderTextColor={'grey'}
+              secureTextEntry={showPassword}
+              underlineColor={'transparent'}
+              right={<TextInput.Icon icon={showPassword ? 'eye-off' : 'eye'} size={20} color={'grey'} onPress={() => setShowPassword(prev => !prev)} />}
+              activeUnderlineColor={'transparent'}
+              left={<TextInput.Icon icon="lock" size={20} color={'grey'} />}
+            />)}>
+          </Controller>
+          {errors.password && renderErrorMsg(errors.password.message)}
         </View>
       </View>
     );
@@ -90,28 +134,43 @@ export default function SignUp() {
       <View style={styles.textInputView}>
         <Text>Confirm Password</Text>
         <View>
-          <TextInput
-            onChangeText={() => {}}
-            style={styles.textInput}
-            placeholder={'Confirm your password'}
-            placeholderTextColor={'grey'}
-            secureTextEntry={true}
-            underlineColor={'transparent'}
-            right={<TextInput.Icon icon={'eye-off'} size={20} color={'grey'} />}
-            activeUnderlineColor={'transparent'}
-            left={<TextInput.Icon icon="lock" size={20} color={'grey'} />}
-          />
+          <Controller
+            control={control}
+            name="confirmPassword"
+            rules={{
+              validate: value =>
+                value === password || 'The passwords do not match'
+            }}
+            render={({ field: { onChange, value } }) => (
+              <TextInput
+                onChangeText={onChange}
+                style={styles.textInput}
+                value={value}
+                placeholder={'Confirm your password'}
+                placeholderTextColor={'grey'}
+                secureTextEntry={showConfirmPassword}
+                underlineColor={'transparent'}
+                right={<TextInput.Icon icon={showConfirmPassword ? 'eye-off' : 'eye'} size={20} color={'grey'} onPress={() => setShowConfirmPassword(prev => !prev)} />}
+                activeUnderlineColor={'transparent'}
+                left={<TextInput.Icon icon="lock" size={20} color={'grey'} />}
+              />)}></Controller>
+          {errors.confirmPassword && renderErrorMsg(errors.confirmPassword.message)}
         </View>
       </View>
     );
+  }
+
+  function renderErrorMsg(message) {
+    return (
+      <Text style={styles.errorMsg}>{message}</Text>)
   }
 
   function renderTnC() {
     return (
       <View style={styles.checkBoxView}>
         <Checkbox
-          status={'checked'}
-          onPress={() => {}}
+          status={agreeTnC ? 'checked' : 'unchecked'}
+          onPress={() => setAgreeTnC(!agreeTnC)}
           color={'#2563eb'}
           style={styles.checkBoxStyle}
         />
@@ -135,11 +194,33 @@ export default function SignUp() {
     return (
       <View style={styles.haveAccountView}>
         <Text style={styles.haveAccount}>Already have an account? </Text>
-        <Pressable onPress={() => navigation.push('SignIn')}>
+        <Pressable onPress={() => navigateToSignInPage()}>
           <Text style={styles.haveAccountBtn}>Sign In</Text>
         </Pressable>
       </View>
     );
+  }
+
+  function onSubmit() {
+    navigateToSignInPage()
+  }
+
+  function navigateToSignInPage() {
+    if (!agreeTnC) {
+      setSnackbarText("Agree to Terms and Conditions to proceed");
+      return;
+    }
+
+    setSnackbarText("Account created successfully, Login now!!");
+    setTimeout(() => {
+      navigation.push('SignIn')
+    }, 1000);
+  }
+
+  function renderSnackbar() {
+    return (
+      <Snackbar visible={SnackbarText.length == 0 ? false : true} onDismiss={() => setSnackbarText('')} duration={1000}>{SnackbarText}</Snackbar>
+    )
   }
 
   function renderCard() {
@@ -152,17 +233,22 @@ export default function SignUp() {
         {renderPasswordTextField()}
         {renderConfirmPasswordTextField()}
         {renderTnC()}
-        <Button buttonText="Create Account" callback={() => {}} />
+        <Button buttonText="Create Account" callback={handleSubmit(onSubmit)} />
+        {renderSnackbar()}
         {renderSignInView()}
       </View>
     );
   }
 
   return (
-    <View style={styles.container}>
-      {renderHeader()}
-      {renderCard()}
-    </View>
+    <KeyboardAvoidingView style={{ flex: 1 }} behavior="padding">
+      <ScrollView keyboardDismissMode='on-drag' keyboardShouldPersistTaps="handled">
+        <View style={styles.container}>
+          {renderHeader()}
+          {renderCard()}
+        </View>
+      </ScrollView>
+    </KeyboardAvoidingView>
   );
 }
 
@@ -213,7 +299,7 @@ const styles = StyleSheet.create({
     paddingTop: 20,
     flexDirection: 'row',
     justifyContent: 'flex-start',
-    alignItems: 'flex-start',
+    alignItems: 'center',
   },
   checkBoxStyle: {
     transform: [{ scale: 1.0 }],
@@ -233,5 +319,8 @@ const styles = StyleSheet.create({
   haveAccountBtn: {
     color: '#2563eb',
     fontWeight: '500',
+  }, errorMsg: {
+    color: 'red',
+    paddingTop: 5,
   },
 });
